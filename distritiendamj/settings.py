5194b04169c644a336c093bb12fe0fc7d4b46046
jhonierp <114941321+jhonierp@users.noreply.github.com>
2023-11-16 10:51:23 -0500

    Update settings.py

--- distritiendamj/settings.py
+++ distritiendamj/settings.py
@@ -102,11 +102,11 @@ DATABASES = {
   }
 }
 
-REST_FRAMEWORK = {
+"""REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (        
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )    
-}
+}"""
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
